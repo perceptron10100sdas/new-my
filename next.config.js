80775a803549587bee7ef7e475e76086f544c87d
Sambhav Das <130726995+perceptron10100sdas@users.noreply.github.com>
2023-08-30 17:32:26 +0530

    build changes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        appDir: true,
+      },
+      eslint: {
+        ignoreDuringBuilds: true,
+    },
     images:
     {
         domains:["e0.pxfuel.com","pbs.twimg.com","mariosfakiolas.com","miro.medium.com","adminlte.io","media.licdn.com","im.rediff.com","static.vecteezy.com"],
