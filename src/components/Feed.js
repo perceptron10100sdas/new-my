7951585a63160163a777585ce7577aceba05c77e
Sambhav Das <130726995+perceptron10100sdas@users.noreply.github.com>
2023-09-01 22:14:45 +0530

    new updates

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -9,23 +9,24 @@ export default function Feed() {
       <h1 className='text-white underline bg-gradient-to-r from-blue-950 via-black to-blue-950'>Available for projects...</h1>
       </div>
 <div className='bg-inherit  md:flex md:justify-evenly'>
-  <div>
-      <Image src="https://www.patterns.dev/img/reactjs/react-logo@3x.svg"
+  <div className='mt-4'>
+    <a href="https://react.dev/" > <Image src="https://www.patterns.dev/img/reactjs/react-logo@3x.svg"
       width={400}
       height={400}
-      className='ring-2 hover:ring-4 rounded-md'
+      className='w-400 h-400 ring-2 hover:ring-4 rounded-md'
       
-      />
+      
+      /></a> 
      
      <Link href="https://react.dev/" className='text-blue-500 text-3xl font-semibold hover:underline'>REACT</Link>
     
      </div>
-     <div className='bg-inherit'>
-      <img src='https://nextjs.org/static/blog/next-13/twitter-card.png'
+     <div className='bg-inherit mt-4'>
+      <a  href="https://nextjs.org/"><img src='https://nextjs.org/static/blog/next-13/twitter-card.png'
       width={400}
       height={400}
       className='ring-2 hover:ring-4 rounded-md'
-      />
+      /></a>
       <Link href="https://nextjs.org/" className='text-white text-3xl font-semibold hover:underline'>NextJs</Link>
       
      </div>
@@ -33,13 +34,13 @@ export default function Feed() {
     </div>
     
     <div className='bg-inherit md:flex md:justify-evenly'>
-      <div>
+      <div className='mt-4 sm:sticky'>
         <div className='flex justify-start'>
-     <Image src="https://simbyone.com/content/images/2023/04/Tailwind_covertailwind_cover.png"
+    <a href="https://tailwindcss.com/"> <Image src="https://simbyone.com/content/images/2023/04/Tailwind_covertailwind_cover.png"
       width={400}
       height={400}
       className='ring-2 hover:ring-4 rounded-md'
-     />
+     /></a>
      </div>
      
      <Link href="https://tailwindcss.com/" className='text-blue-500 text-3xl hover:overline'>Tailwind CSS</Link>
