0760c46b8b4b0625f3eae2e6e75386cc3cf4544b
Sambhav Das <130726995+perceptron10100sdas@users.noreply.github.com>
2023-09-01 22:45:36 +0530

    changes

--- src/components/Project.js
+++ src/components/Project.js
@@ -19,7 +19,7 @@ export default function project() {
       className='ring-2 hover:ring-red-500 rounded-md'
       />
       </a>
-      <div className='flex justify-end'><Link href="https://github.com/perceptron10100sdas/starbucks" className='font-bold text-white bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-black py-1 px-1 rounded-lg mr-1'>Source Code for Google</Link></div>
+      <div className='flex justify-end'><a href="https://github.com/perceptron10100sdas/starbucks" className='font-bold text-white bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-black py-1 px-1 rounded-lg mr-1'><img src='https://w7.pngwing.com/pngs/240/158/png-transparent-social-media-computer-icons-github-fork-github-purple-text-social-media.png' width={50} height={50}/></a></div>
       <Link href="https://google-clone-lyart-gamma.vercel.app/" className='text-2xl text-blue-950 hover:text-yellow-600 font-semibold bg-yellow-500 hover:bg-blue-950 rounded-md hover:underline'>Google clone </Link>
       <p className='text-white'>A Google clone developed by me using nextjs13 and tailwind css and powered by google search API</p>
       </div>
@@ -31,9 +31,8 @@ export default function project() {
       
         className='ring-2 hover:ring-black rounded-none'/></a>
       
-        <div className='flex justify-end'><Link href="https://github.com/perceptron10100sdas/imdb" className='font-bold text-black bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500py-1 px-1 rounded-lg mr-1'>Source Code for Imdb</Link><img src='https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU'
-         width={50}
-         height={50}/></div>
+        <div className='flex justify-end'><Link href="https://github.com/perceptron10100sdas/imdb" className='font-bold text-black bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500py-1 px-1 rounded-lg mr-1'><img src='https://w7.pngwing.com/pngs/240/158/png-transparent-social-media-computer-icons-github-fork-github-purple-text-social-media.png' width={50} height={50}/></Link>
+        </div>
         <Link href="https://imdb-perceptron10100.vercel.app/"  className="font-bold bg-amber-500 py-1 px-1 rounded-lg mr-1">IMDB clone</Link>
         <p className='text-white'>Imdb clone made using nextjs 13 and tailwind css and data fetched by tmdb</p>
         </div>  
@@ -44,7 +43,7 @@ width={500}
 height={500}
 className='ring-2 hover:ring-green-950 rounded-none'/></a>
 
-<div className='flex justify-end'><Link href="https://github.com/perceptron10100sdas/starbucks" className='font-bold text-black bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-black py-1 px-1 rounded-lg mr-1'>Source Code for Starbucks</Link></div>
+<div className='flex justify-end'><Link href="https://github.com/perceptron10100sdas/starbucks" className='font-bold text-black bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-black py-1 px-1 rounded-lg mr-1'><img src='https://w7.pngwing.com/pngs/240/158/png-transparent-social-media-computer-icons-github-fork-github-purple-text-social-media.png' width={50} height={50}/></Link></div>
 <Link href="https://astounding-kringle-914635.netlify.app/" className='text-2xl text-white bg-gradient-to-r from-green-950 to-black py-1 px-1 rounded-md font-semibold'>Starbucks Clone</Link>
 <p className='text-white'>Starbucks clone made using React and inline css</p>
 </div>  
