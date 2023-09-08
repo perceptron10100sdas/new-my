6dd574817a990788adc642a1c2f7269287f4f12d
Sambhav Das <130726995+perceptron10100sdas@users.noreply.github.com>
2023-09-08 09:47:58 +0530

    fixed the contacts

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -5,20 +5,55 @@ export default function contact() {
   return (
     <div>
        <div className='bg-gradient-to-r from-green-500 via-black to-red-500'>
-    <h1 className='text-3xl text-white flex justify-center'>Let's connect...</h1>
+   <h1 className='text-3xl text-center text-white animate-bounce'>Contact INfo</h1>
     </div>
    
     <div className='flex justify-between bg-gradient-to-r from-indigo-700 to-black'>
+      <div className='flex'>
     <a href='https://www.linkedin.com/in/sambhav-das-630ba1278/'> <Image src="https://static.vecteezy.com/system/resources/previews/018/930/587/original/linkedin-logo-linkedin-icon-transparent-free-png.png"
       width="90"
       height="90"/>
       </a>
+      <a href='https://www.linkedin.com/in/sambhav-das-630ba1278/'> <div className='flex bg-blue-950 rounded-md ring-2 hover:ring-black mt-2'>
+   
+      <div >
+      <h1 className='text-white text-2xl mt-2 mx-10'>Sambhav Das</h1>
+      <h2 className='text-white mx-10'>Let's connect on LinkedIn
+      </h2>
+      
+      </div>
+
+      </div></a></div>
+   
+   
+    </div>
+    <div className='flex justify-end'>
+    <a href='https://twitter.com/imSambhavDas'> <div className='flex bg-blue-950 rounded-md ring-2 hover:ring-black'>
+    <Image src="https://pbs.twimg.com/profile_images/1678817133051138048/j6WFI0kj_400x400.jpg"
+width="90"
+height="90"
+className='rounded-full ring-black'
+
+/>
+      <div>
+      <h1 className='text-white text-2xl mt-2 mx-10'>Sambhav Das</h1>
+      <h2 className='text-white mx-10'>@imSambhavDas</h2>
+      
+      </div>
+
+      </div></a>
     <a href='https://twitter.com/imSambhavDas'> <Image src="https://im.rediff.com/news/2023/jul/24twitter.jpg?w=670&h=900"
       width="100"
       height="100"
-      className='mt-3'/></a> 
-   
-    </div>
+      className='mt-3'/></a>
+     
+      
+      
+      
+      
+      
+      
+       </div>
     </div>
   )
 }
--- src/app/skills/page.js
+++ src/app/skills/page.js
@@ -8,7 +8,8 @@ export default function skills() {
       <h1 className='text-white underline bg-gradient-to-r from-blue-950 via-black to-blue-950 hover:animate-ping'>Available for projects...</h1>
       </div>
 <div className='bg-inherit  md:flex md:justify-evenly'>
-  <div className='mt-4'>
+  <div className='mt-5 '>
+    <div className='flex justify-center '>
     <a href="https://react.dev/" > <Image src="https://www.patterns.dev/img/reactjs/react-logo@3x.svg"
       width={400}
       height={400}
@@ -16,18 +17,18 @@ export default function skills() {
 
       
       
-      /></a> 
+      /></a> </div>
      
-     <Link href="https://react.dev/" className='text-blue-500 text-2xl font-semibold hover:underline mt-2  '>REACT</Link>
+     <Link href="https://react.dev/" className='text-blue-500 text-2xl font-semibold hover:underline mt-3 mx-2  '>REACT</Link>
     
      </div>
      <div className='bg-inherit mt-4'>
-      <a  href="https://nextjs.org/"><img src='https://nextjs.org/static/blog/next-13/twitter-card.png'
+      <a  href="https://nextjs.org/"> <div className='flex justify-center mx-2 mt-2'><img src='https://nextjs.org/static/blog/next-13/twitter-card.png'
       width={400}
       height={400}
       className='ring-2 hover:ring-4 rounded-md '
-      /></a>
-      <Link href="https://nextjs.org/" className='text-white text-2xl font-semibold hover:underline mt-2'>NextJs</Link>
+      /></div></a>
+      <Link href="https://nextjs.org/" className='text-white text-2xl font-semibold hover:underline mt-3 mx-2'>NextJs</Link>
       
      </div>
      
@@ -36,14 +37,14 @@ export default function skills() {
     <div className='bg-inherit md:flex md:justify-evenly'>
       <div className='mt-4 sm:sticky'>
         <div className='flex '>
-    <a href="https://tailwindcss.com/"> <Image src="https://simbyone.com/content/images/2023/04/Tailwind_covertailwind_cover.png"
+    <a href="https://tailwindcss.com/"> <div className='flex justify-center mt-2 mx-2'><Image src="https://simbyone.com/content/images/2023/04/Tailwind_covertailwind_cover.png"
       width={400}
       height={400}
       className='ring-2 hover:ring-4 rounded-md'
-     /></a>
+     /></div></a>
      </div>
      
-     <Link href="https://tailwindcss.com/" className='text-blue-500 text-2xl hover:overline mt-2'>Tailwind CSS</Link>
+     <Link href="https://tailwindcss.com/" className='text-blue-500 text-2xl hover:overline mt-3 mx-2'>Tailwind CSS</Link>
      
      </div>
      <div> 
