5d02c643b6759495b7f92834a2516f4773d95eb8
Sambhav Das <130726995+perceptron10100sdas@users.noreply.github.com>
2023-09-18 11:45:35 +0530

    changes in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,16 +7,16 @@ export default function Footer() {
   return (
     <div className=' bg-black '>
         <div className='flex justify-evenly '>
-          <Link href="/skills"  
+        <div className='bg-inherit hover:bg-blue-950 ring-1 mt-3 hover:ring-white '> <Link href="/skills"  
          
           className='text-blue-500 mx-5 my-6 flex items-start text-2xl rounded-lg font-semibold hover:text-white hover:animate-bounce'
           
        
           
-          ><h1>Technologies and Skills</h1></Link>
+          ><h1>Skills</h1></Link></div>
           <div className='flex items-center'>
-      <Link href="/project"  className='text-blue-500 mx-4 my-6 flex items-start text-2xl rounded-lg font-semibold  hover:text-white hover:animate-bounce '><h1>My Portfolio</h1></Link></div>
-     <Link href="/contact"  className='text-blue-500 mx-4 my-6 flex items-start text-2xl rounded-lg font-semibold  hover:text-white hover:animate-bounce'><h1>Contact</h1></Link>
+          <div className='bg-inherit hover:bg-blue-950 ring-1 mt-3 hover:ring-white '><Link href="/project"  className='text-blue-500 mx-4 my-6 flex items-start text-2xl rounded-lg font-semibold  hover:text-white hover:animate-bounce '><h1>My Portfolio</h1></Link></div></div>
+    <div className='bg-inherit hover:bg-blue-950 ring-1 mt-3 hover:ring-white '> <Link href="/contact"  className='text-blue-500 mx-4 my-6 flex items-start text-2xl font-semibold  hover:text-white hover:animate-bounce'><h1>Contact</h1></Link></div>
      </div>
      <h2 className='text-2xl text-center text-blue-950 mt3' >Click to navigate to different sections</h2>
 <Link href="/" className='text-white flex justify-center mt-7'>Return to Home page</Link>
