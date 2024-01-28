9574fdfe3515483807ab123f0e1a694aed8c6ba1
Sambhav Das <130726995+perceptron10100sdas@users.noreply.github.com>
2024-01-28 23:46:45 +0530

    thin

--- src/app/backend/page.js
+++ src/app/backend/page.js
@@ -27,17 +27,17 @@ export default function page() {
           </div>
       
           <div className='grid gap-7'>
-         <div className='flex justify-center gap-x-10'>  <SiNodedotjs className='text-8xl text-green-500'/><motion.h1 className='text-8xl text-white font-light'  initial={{ x:-100}} animate={{ x: 0}}
+         <div className='flex justify-center gap-x-10'>  <SiNodedotjs className='text-8xl text-green-500'/><motion.h1 className='text-8xl text-white font-thin'  initial={{ x:-100}} animate={{ x: 0}}
   transition={{duration:3, ease:"anticipate" }}>NodeJS</motion.h1></div>
        
        
   
-       <div className='flex justify-center gap-x-10'>  <SiExpress className='text-8xl text-slate-500'/><motion.h1 className='text-8xl text-white font-light'  initial={{ x:-100}} animate={{ x: 0}}
+       <div className='flex justify-center gap-x-10'>  <SiExpress className='text-8xl text-slate-500'/><motion.h1 className='text-8xl text-white font-thin'  initial={{ x:-100}} animate={{ x: 0}}
   transition={{duration:3.5, ease:"anticipate" }}>ExpressJs</motion.h1></div>
 
-<div className='flex justify-center gap-x-10'>  <SiFirebase className='text-8xl text-orange-500'/><motion.h1 className='text-8xl text-white font-light'  initial={{ x:-100}} animate={{ x: 0}}
+<div className='flex justify-center gap-x-10'>  <SiFirebase className='text-8xl text-orange-500'/><motion.h1 className='text-8xl text-white font-thin'  initial={{ x:-100}} animate={{ x: 0}}
   transition={{duration:4, ease:"anticipate" }}>Firebase<p className='text-white text-xl'>Database and Auth</p></motion.h1></div>
-    <div className='flex justify-center gap-x-10'>  <SiMongodb className='sm:text-6xl md:text-8xl text-green-500'/><motion.h1 className='text-8xl text-white font-light'  initial={{ x:-100}} animate={{ x: 0}}
+    <div className='flex justify-center gap-x-10'>  <SiMongodb className='sm:text-6xl md:text-8xl text-green-500'/><motion.h1 className='text-8xl text-white font-thin'  initial={{ x:-100}} animate={{ x: 0}}
   transition={{duration:4.3, ease:"anticipate" }}>MongoDB<p className='text-white text-xl'>Database</p></motion.h1></div>
     
     </div></div>
