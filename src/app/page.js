249d0617593a55abef7333bca4859853754730d6
Sambhav Das <130726995+perceptron10100sdas@users.noreply.github.com>
2024-01-29 22:15:38 +0530

    new

--- src/app/page.js
+++ src/app/page.js
@@ -26,7 +26,7 @@ import { Html } from '@react-three/drei'
 import {  useFrame } from "@react-three/fiber";
 import {  Sphere } from "@react-three/drei";
 import { pointsInner, pointsOuter } from "./../components/utils";
-import ParticleRing from './Particlering'
+
 
 
 
